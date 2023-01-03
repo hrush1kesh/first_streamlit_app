0c9c467d14e745b4a4b88271fbce08f55777fdd7
hrush1kesh <121569994+hrush1kesh@users.noreply.github.com>
2023-01-03 15:52:58 +0530

    Update streamlit_app.py
    using pandas to read json in normalized form
    using pd dataframe to display normalized form into table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# using pandas to read json in normalized form
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# using pd dataframe to display normalized form into table
+streamlit.dataframe(fruityvice_normalized)
